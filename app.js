3efe868b566c5654332d3e9a0b56e78b9ade6abe
jtw007 <joshwu095@gmail.com>
2023-05-09 14:53:25 -0700

    added some functions

--- app.js
+++ app.js
@@ -1,20 +1,55 @@
-const player = {
-    currentChoice: null
-}
-const computer = {
-    currentChoice: null
-}
-
 const choices = ["Rock", "Paper", "Scissors"]
-const randomIndex = Math.floor(Math.random() * choices.length);
+const randomIndex = Math.floor(Math.random() * 3);
+
+// ==computer and player functions== 
+// const player = () => {
+//     currentChoice: null
+// }
+
+const computer = () => {
+    randomIndex
+    const computerChoice = choices[randomIndex]
+    return computerChoice
+}
+computer()
+
+const playRound = (player, computer) => {
+    if (player.currentChoice === choices[0] && computer.currentChoice === choices[1]) {
+        console.log("You lose! Paper beats Rock!");
+    }
+    else if (player.currentChoice === choices[0] && computer.currentChoice === choices[2]) {
+        console.log('You win! Rock beats Scissors!');
+    }      
+    else if (player.currentChoice === choices[0] && computer.currentChoice === choices[0]) {
+        console.log("It's a draw!");
+    } 
+    else if (player.currentChoice === choices[1] && computer.currentChoice === choices[2]) {
+        console.log("You lose! Scissors beats Paper!");
+    }  
+    else if (player.currentChoice === choices[1] && computer.currentChoice === choices[0]){
+        console.log('You win! Paper beats Rock!');
+    }
+    else if (player.currentChoice === choices[1] && computer.currentChoice === choices[1]) {
+        console.log("It's a draw!");
+    }
+    else if (player.currentChoice === choices[2] && computer.currentChoice === choices[0]) {
+        console.log("You lose! Rock beats Scissors!");
+    }
+    else if (player.currentChoice === choices[2] && computer.currentChoice === choices[1]) {
+        console.log("You win! Scissors beats Paper!");
+    }
+    else if (player.currentChoice === choices[2] && computer.currentChoice ===choices[2]) {
+        console.log("It's a draw!");
+    }
+}
 
 
-//query selectors
+//==query selectors==
 rock = document.querySelector('#rock')
 paper = document.querySelector('#paper')
 scissors = document.querySelector('#scissors')
 
-//event listeners
+//==event listeners==
 if (rock) {
     rock.addEventListener("click", () => {
         console.log('clicked rock')
