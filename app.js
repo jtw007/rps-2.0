e7bceb21d279df32afe651502b1ea6493a0f1374
jtw007 <joshwu095@gmail.com>
2023-05-16 15:14:02 -0700

    game is functional, messages display on html side

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 let playerScore = 0
 let computerScore = 0
+//==query selectors==
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
+const outcomeDiv = document.querySelector('.outcome')
 
 const choices = ["Rock", "Paper", "Scissors"]
 const randomIndex = Math.floor(Math.random() * 3);
@@ -10,71 +15,95 @@ const computerChoice = () => {
     return computerChoice
 }
 
+
 const playRound = (player, computer) => {
     if (player === choices[0] && computer === choices[1]) {
         computerScore++
-        return "You lose! Paper beats Rock!"
+        const p = document.createElement('p')
+        p.innerText = "You lose! Paper beats Rock!"
+        outcomeDiv.appendChild(p)
     }
     else if (player === choices[0] && computer === choices[2]) {
         playerScore++
-        return 'You win! Rock beats Scissors!'
+        const p = document.createElement('p')
+        p.innerText = 'You win! Rock beats Scissors!'
+        outcomeDiv.appendChild(p)
     }      
     else if (player === choices[0] && computer === choices[0]) {
-        return "It's a draw!"
+        const p = document.createElement('p')
+        p.innerText = "It's a draw!"
+        outcomeDiv.appendChild(p)
     } 
     else if (player === choices[1] && computer === choices[2]) {
         computerScore++
-        return "You lose! Scissors beats Paper!"
+        const p = document.createElement('p')
+        p.innerText = "You lose! Scissors beats Paper!"
+        outcomeDiv.appendChild(p)
     }  
     else if (player === choices[1] && computer === choices[0]){
         playerScore++
-        return 'You win! Paper beats Rock!'
+        const p = document.createElement('p')
+        p.innerText = 'You win! Paper beats Rock!'
+        outcomeDiv.appendChild(p)
     }
     else if (player === choices[1] && computer === choices[1]) {
-        return "It's a draw!"
+        const p = document.createElement('p')
+        p.innerText = "It's a draw!"
+        outcomeDiv.appendChild(p)
     }
     else if (player === choices[2] && computer === choices[0]) {
         computerScore++
-        return "You lose! Rock beats Scissors!"
+        const p = document.createElement('p')
+        p.innerText = "You lose! Rock beats Scissors!"
+        outcomeDiv.appendChild(p)
     }
     else if (player === choices[2] && computer === choices[1]) {
         playerScore++
-        return "You win! Scissors beats Paper!"
+        const p = document.createElement('p')
+        p.innerText = "You win! Scissors beats Paper!"
+        outcomeDiv.appendChild(p)
     }
     else if (player === choices[2] && computer ===choices[2]) {
-        return "It's a draw!"
+        const p = document.createElement('p')
+        p.innerText = "It's a draw!"
+        outcomeDiv.appendChild(p)
     }
 }
+// event listeners
+rock.addEventListener('click', () => {
+    const computer = computerChoice()
+    const player = 'Rock'
+    playRound(player, computer)
+})
 
+paper.addEventListener('click', () => {
+    const computer = computerChoice()
+    const player = 'Paper'
+    playRound(player, computer)
+})
 
-const game = () => {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Make your choice', 'Rock, Paper, Scissors').toLowerCase()
-        const computerSelection = computerChoice()
-        playRound(playerSelection, computerSelection)
-    }
-    if (playerScore > computerScore) {
-        return 'You are victorious!'
-    } else if (playerScore < computerScore) {
-        return 'You lost to a computer lmao'
-    } else {
-        return 'You tied with the computer'
-    }
-}
+scissors.addEventListener('click', () => {
+    const computer = computerChoice()
+    const player = 'Scissors'
+    playRound(player, computer)
+})
 
-console.log(game())
+// const game = () => {
+//     for (let i = 0; i < 5; i++) {
+//         const playerSelection = prompt('Make your choice', 'Rock, Paper, Scissors').toLowerCase()
+//         const computerSelection = computerChoice()
+//         playRound(playerSelection, computerSelection)
+//     }
+//     if (playerScore > computerScore) {
+//         return 'You are victorious!'
+//     } else if (playerScore < computerScore) {
+//         return 'You lost to a computer lmao'
+//     } else {
+//         return 'You tied with the computer'
+//     }
+// }
 
-//==query selectors==
-rock = document.querySelector('#rock')
-paper = document.querySelector('#paper')
-scissors = document.querySelector('#scissors')
-
-//==event listeners==
-if (rock) {
-    rock.addEventListener("click", () => {
-        console.log('clicked rock')
-    } 
-)}
+// console.log(game())
 
   
 // computer.currentChoice = choices[randomIndex];
